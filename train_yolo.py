f3b35a2d46cad8f523748e0c3634e7df27808344
vtphatt2 <thinhphat544@gmail.com>
2024-12-23 19:42:10 +0700

    best_sbd_md.pt

--- train_yolo.py
+++ train_yolo.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO("yolov5m.pt") 
+model = YOLO("yolov8m.pt") 
 
 # Train
-model.train(data='data.yaml', epochs=1, imgsz=640, batch=16, device='0') 
+model.train(data='data.yaml', 
+            epochs=50, 
+            imgsz=1024, 
+            batch=16, 
+            device='2') 
