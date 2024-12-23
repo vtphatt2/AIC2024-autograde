cc47687c112bde066e735a41de3f1d6298ea3057
vtphatt2 <thinhphat544@gmail.com>
2024-12-23 20:10:42 +0700

    add submit.py

---
+++ submit.py
@@ -0,0 +1,23 @@
+from glob import glob
+from ultralytics import YOLO
+import os
+
+
+model_for_sbd_mdt = YOLO('best_sbd_mdt.pt')
+# model_for_part_1 = YOLO('best_part_2.pt')
+# model_for_part_2 = YOLO('best_part_3.pt')
+# model_for_part_3 = YOLO('best_part_4.pt')
+
+
+test_image_files = glob(os.path.join('testset1', 'images', '*.jpg'))
+
+result_path = os.path.join('submission', 'result.txt')
+
+
+def process_and_write_to_file(results):
+
+
+
+for image_path in test_image_files:
+    results = model_for_sbd_mdt (image_path)
+    process_and_write_to_file(results)
