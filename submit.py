0f8d72370b474be22e13bf135a83c391b11bcac7
vtphatt2 <thinhphat544@gmail.com>
2024-12-25 00:06:39 +0700

    finish submit.py

--- submit.py
+++ submit.py
@@ -13,6 +13,31 @@ testset_image_files.sort()
 os.makedirs('submission', exist_ok=True)
 # submit_sbd_mdt(testset_image_files)
 # submit_part1(testset_image_files)
-submit_part2(testset_image_files)
+# submit_part2(testset_image_files)
 # submit_part3(testset_image_files)
 
+
+input_files = [
+    os.path.join("submission", "results_sbd_mdt.txt"),
+    os.path.join("submission", "results_part1.txt"),
+    os.path.join("submission", "results_part2.txt"),
+    os.path.join("submission", "results_part3.txt"),
+]
+output_file = os.path.join("submission", "results.txt")
+
+data = {}
+
+for file in input_files:
+    with open(file, "r") as f:
+        for line in f:
+            img_name, values = line.strip().split(" ", 1)
+
+            if img_name not in data:
+                data[img_name] = [values] 
+            else:
+                data[img_name].append(values) 
+
+with open(output_file, "w") as f:
+    for img_name, values in data.items():
+        merged_line = f"{img_name} {' '.join(values)}\n"
+        f.write(merged_line)
