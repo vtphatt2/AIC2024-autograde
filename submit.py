9ad3490da9bdea7ea3741243798d91c9bcbb8a29
Nguyễn Ngọc Cảnh <130559533+Orange1301@users.noreply.github.com>
2024-12-25 07:33:07 +0700

    result part 3

--- submit.py
+++ submit.py
@@ -1,7 +1,7 @@
-from submit_sbd_mdt import submit_sbd_mdt
-from submit_part1 import submit_part1
-from submit_part2 import submit_part2
-from submit_part3 import submit_part3
+# from submit_sbd_mdt import submit_sbd_mdt
+# from submit_part1 import submit_part1
+# from submit_part2 import submit_part2
+# from submit_part3 import submit_part3
 from glob import glob 
 import os
 
@@ -12,7 +12,7 @@ testset_image_files.sort()
 
 os.makedirs('submission', exist_ok=True)
 # submit_sbd_mdt(testset_image_files)
-submit_part1(testset_image_files)
+# submit_part1(testset_image_files)
 # submit_part2(testset_image_files)
 # submit_part3(testset_image_files)
 
